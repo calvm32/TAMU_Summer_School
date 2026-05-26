9e9830b9bfd60e0a6d841fea6b5a5ecd88b933bd
Calum Heldt <cal.muon32@gmail.com>
2026-05-25 22:45:28 -0500

    testing exact solution

--- elasticity_project/finite_difference_steps.py
+++ elasticity_project/finite_difference_steps.py
@@ -9,8 +9,8 @@ def linear_center_diff_step(c, U, V, n, xs, ts, epsilon = 0):
     U_next = np.zeros(total_points+1)
     V_next = np.zeros(total_points+1)
 
-    h = (xs[-1] - xs[0])/len(xs)
-    tau = (ts[-1] - ts[0])/len(ts)
+    h = xs[1] - xs[0]
+    tau = ts[1] - ts[0]
     for i in range(total_points+1):
 
         if i == 0:
@@ -39,8 +39,8 @@ def linear_forward_diff_step(c, U, V, n, xs, ts, epsilon = 0):
     U_next = np.zeros(total_points+1)
     V_next = np.zeros(total_points+1)
 
-    h = (xs[-1] - xs[0])/len(xs)
-    tau = (ts[-1] - ts[0])/len(ts)
+    h = xs[1] - xs[0]
+    tau = ts[1] - ts[0]
     for i in range(total_points+1):
 
         if i == 0:
@@ -67,7 +67,7 @@ def linear_forward_diff_step(c, U, V, n, xs, ts, epsilon = 0):
 # =================================================================================================
 # =================================================================================================
 
-def nonlinear_center_diff_step(c, U, V, n, xs, ts, epsilon = 0):
+def nonlinear_center_diff_step(c, U, V, n, f, xs, ts, epsilon = 0):
 
     total_times = len(ts)-1
     total_points = len(xs)-1
@@ -76,35 +76,38 @@ def nonlinear_center_diff_step(c, U, V, n, xs, ts, epsilon = 0):
     U_next = np.zeros(total_points+1)
     V_next = np.zeros(total_points+1)
 
-    h = (xs[-1] - xs[0])/len(xs)
-    tau = (ts[-1] - ts[0])/len(ts)
+    h = xs[1] - xs[0]
+    tau = ts[1] - ts[0]
     for i in range(total_points+1):
 
         if i == 0:
-            dx_v = (V[n,i+1] - V[n,i])/h
-            dx_u = (U[n,i+1] - U[n,i])/h
-            U_next[i] = U[n-1,i] - (2*tau)*dx_v
-            V_next[i] = V[n-1,i] - (c**2)*(2*tau)*dx_u*(1+dx_u)*(1+0.5*dx_u)
+            forcing = 2*tau*f(ts[n],xs[i])
+            v_x = (V[n,i+1] - V[n,i])/h
+            u_x = (U[n,i+1] - U[n,i])/h
+            U_next[i] = U[n-1,i] - (2*tau)*v_x
+            V_next[i] = V[n-1,i] - (c**2)*(2*tau)*u_x*(1+u_x)*(1+0.5*u_x) + forcing
         elif i == total_points:
-            dx_v = (V[n,i] - V[n,i-1])/h
-            dx_u = (U[n,i] - U[n,i-1])/h
-            U_next[i] = U[n-1,i] - (2*tau)*dx_v
-            V_next[i] = V[n-1,i] - (c**2)*(2*tau)*dx_u*(1+dx_u)*(1+0.5*dx_u)
+            forcing = 2*tau*f(ts[n],xs[i])
+            v_x = (V[n,i] - V[n,i-1])/h
+            u_x = (U[n,i] - U[n,i-1])/h
+            U_next[i] = U[n-1,i] - (2*tau)*v_x
+            V_next[i] = V[n-1,i] - (c**2)*(2*tau)*u_x*(1+u_x)*(1+0.5*u_x) + forcing
         else:
+            forcing = 2*tau*f(ts[n],xs[i])
             stability_term1 = 2*epsilon*tau/(h**2)*(V[n,i+1] - 2*V[n,i] + V[n,i-1])
             stability_term2 = 2*epsilon*tau/(h**2)*(U[n,i+1] - 2*U[n,i] + U[n,i-1])
 
-            dx_v = (V[n,i+1] - V[n,i-1])/(2*h)
-            dx_u = (U[n,i+1] - U[n,i-1])/(2*h)
+            v_x = (V[n,i+1] - V[n,i-1])/(2*h)
+            u_x = (U[n,i+1] - U[n,i-1])/(2*h)
             
-            U_next[i] = U[n-1,i] - (2*tau)*dx_v + stability_term1
-            V_next[i] = V[n-1,i] - (c**2)*(2*tau)*dx_u*(1+dx_u)*(1+0.5*dx_u) + stability_term2
+            U_next[i] = U[n-1,i] - (2*tau)*v_x + stability_term1
+            V_next[i] = V[n-1,i] - (c**2)*(2*tau)*u_x*(1+u_x)*(1+0.5*u_x) + stability_term2 + forcing
 
     #np.savetxt('output.txt', U, fmt='%.2f', delimiter=',')
 
     return U_next, V_next
 
-def nonlinear_forward_diff_step(c, U, V, n, xs, ts, epsilon = 0):
+def nonlinear_forward_diff_step(c, U, V, n, f, xs, ts, epsilon = 0):
 
     total_times = len(ts)-1
     total_points = len(xs)-1
@@ -113,29 +116,32 @@ def nonlinear_forward_diff_step(c, U, V, n, xs, ts, epsilon = 0):
     U_next = np.zeros(total_points+1)
     V_next = np.zeros(total_points+1)
 
-    h = (xs[-1] - xs[0])/len(xs)
-    tau = (ts[-1] - ts[0])/len(ts)
+    h = xs[1] - xs[0]
+    tau = ts[1] - ts[0]
     for i in range(total_points+1):
 
         if i == 0:
-            dx_v = (V[n,i+1] - V[n,i])/h
-            dx_u = (U[n,i+1] - U[n,i])/h
-            U_next[i] = U[n,i] - (tau)*dx_v
-            V_next[i] = V[n,i] - (c**2)*(tau)*dx_u
+            forcing = tau*f(ts[n],xs[i])
+            v_x = (V[n,i+1] - V[n,i])/h
+            u_x = (U[n,i+1] - U[n,i])/h
+            U_next[i] = U[n,i] - (tau)*v_x
+            V_next[i] = V[n,i] - (c**2)*(tau)*u_x*(1+u_x)*(1+0.5*u_x) + forcing
         elif i == total_points:
-            dx_v = (V[n,i] - V[n,i-1])/h
-            dx_u = (U[n,i] - U[n,i-1])/h
-            U_next[i] = U[n,i] - (tau)*dx_v
-            V_next[i] = V[n,i] - (c**2)*(tau)*dx_u
+            forcing = tau*f(ts[n],xs[i])
+            v_x = (V[n,i] - V[n,i-1])/h
+            u_x = (U[n,i] - U[n,i-1])/h
+            U_next[i] = U[n,i] - (tau)*v_x
+            V_next[i] = V[n,i] - (c**2)*(tau)*u_x*(1+u_x)*(1+0.5*u_x) + forcing
         else:
+            forcing = tau*f(ts[n],xs[i])
             stability_term1 = 2*epsilon*tau/(2*h**2)*(V[n,i+1] - 2*V[n,i] + V[n,i-1])
             stability_term2 = 2*epsilon*tau/(2*h**2)*(U[n,i+1] - 2*U[n,i] + U[n,i-1])
 
-            dx_v = (V[n,i+1] - V[n,i-1])/(2*h)
-            dx_u = (U[n,i+1] - U[n,i-1])/(2*h)
+            v_x = (V[n,i+1] - V[n,i-1])/(2*h)
+            u_x = (U[n,i+1] - U[n,i-1])/(2*h)
             
-            U_next[i] = U[n,i] - (tau)*dx_v + stability_term1
-            V_next[i] = V[n,i] - (c**2)*(tau)*dx_u*(1+dx_u)*(1+0.5*dx_u) + stability_term2
+            U_next[i] = U[n,i] - (tau)*v_x + stability_term1
+            V_next[i] = V[n,i] - (c**2)*(tau)*u_x*(1+u_x)*(1+0.5*u_x) + stability_term2 + forcing
 
     np.savetxt('output.txt', U, fmt='%.2f', delimiter=',')
 
--- elasticity_project/solve_equation.py
+++ elasticity_project/solve_equation.py
@@ -6,10 +6,11 @@ from IPython.display import HTML
 from elasticity_project.finite_difference_steps import *
 
 """
-solve { u_t + v_x = 0, v_t + c^2u_x = 0 } on [a,b]
+solve { u_t + v_x = 0, v_t + c^2u_x(1+u_x)(1+0.5u_x) = F } on [a,b]
+equivalent form: u_tt - c^2partial_x ( u_x(1+u_x)(1+0.5u_x) ) = F_t
 """
 
-def solve(c, alpha, beta, u_0, v_0, xs, ts, epsilon = 0):
+def solve(c, alpha, beta, u_0, v_0, f, xs, ts, epsilon = 0):
 
     total_times = len(ts)-1
     total_points = len(xs)-1
@@ -23,12 +24,12 @@ def solve(c, alpha, beta, u_0, v_0, xs, ts, epsilon = 0):
         U[0, i] = u_0(xs[i])
         V[0, i] = v_0(xs[i])
 
-    U[1,:], V[1,:] = nonlinear_forward_diff_step(c, U, V, 0, xs, ts, epsilon)
+    U[1,:], V[1,:] = nonlinear_forward_diff_step(c, U, V, 0, f, xs, ts, epsilon)
 
     # time-stepping
-    tau = (T-t0)/(total_times+1)
+    tau = ts[1] - ts[0]
     for n in range(1, total_times):
-        U[n+1,:], V[n+1,:] = nonlinear_center_diff_step(c, U, V, n, xs, ts, epsilon)
+        U[n+1,:], V[n+1,:] = nonlinear_center_diff_step(c, U, V, n, f, xs, ts, epsilon)
 
     return U, V
 
@@ -51,31 +52,41 @@ if __name__ == "__main__":
 
     h = (b - a)/(total_points+1)
     xs = [a + i*h for i in range(total_points + 1)]
-    epsilon = 0.001*h**2 # stability term
+    epsilon = 0 #2*h**2 # stability term
     
     # time discretization
     total_times = 1000
     t0 = 0
     T = 100
 
-    u_0 = lambda x: np.exp(-x**2)
-    v_0 = lambda x: 0
-
-    tau = 0.1 #min((1/c)*h, (T-t0)/total_times)
+    tau = min((1/c)*h, (T-t0)/total_times)
     ts = []
     time = t0
     while time <= T:
         ts.append(time)
         time += tau
 
+    # --------------------
+    # function definitions
+    # --------------------
+
+    u_0 = lambda x: np.sin(np.pi*x)
+    v_0 = lambda x: c*np.sin(np.pi*x)
+
+    u_x  = lambda t,x: np.pi*np.cos(np.pi*(x - c*t))
+    v_t  = lambda t,x: -c**2*np.pi*np.cos(np.pi*(x - c*t))
+
+    f = lambda t,x: v_t(t,x) +c**2*u_x(t,x)*(1+u_x(t,x))*(1+0.5*u_x(t,x))
+
     # -------------------------
     # compute approx. and exact
     # -------------------------
 
     # approximate solution
-    U, V = solve(c, alpha, beta, u_0, v_0, xs, ts, epsilon)
+    U, V = solve(c, alpha, beta, u_0, v_0, f, xs, ts, epsilon)
 
-    u_exact = lambda x,t: 0.5*(u_0(x+c*t)+ u_0(x-c*t))
+    u_exact = lambda t,x: np.sin(np.pi*(x-c*t))
+    U_exact = np.array([[u_exact(t, x) for x in xs] for t in ts])
 
     # --------------------
     # animate the solution
@@ -86,19 +97,22 @@ if __name__ == "__main__":
     U_max = np.max(U)
     U_min = np.min(U)
     abs_U_max = np.max(np.abs(U))
-    y_min = 1 # U_min - 0.1 * abs_U_max
-    y_max = -1 #U_max + 0.1 * abs_U_max
+    y_min = -1 # U_min - 0.1 * abs_U_max
+    y_max = 1 #U_max + 0.1 * abs_U_max
     ax.set(
         ylim=[y_min, y_max], xlabel="x", ylabel="u(x,t)", title=f"t = {ts[0]:2f}"
     )
-    line = ax.plot(xs, U[0,:])[0]
+    line, = ax.plot(xs, U[0,:], label="U (approx)")
+    line_exact, = ax.plot(xs, U_exact[0,:], label="U_exact", linestyle="--")
 
     # update the plot each frame
     def update(frame):
         line.set_ydata(U[frame,:])
+        line_exact.set_ydata(U_exact[frame,:])
         ax.set(title=f"t = {ts[frame]:2f}")
-        return line
+        return line, line_exact
 
     # create the animation
     ani = animation.FuncAnimation(fig=fig, func=update, frames=total_times + 1, interval=30, blit=False)
+    plt.legend()
     plt.show()
